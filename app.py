6d54c3de92be748b71b49cb5433061d4ed0b9a64
Shahnab Ahmed <Shad0ws@users.noreply.huggingface.co>
2023-03-04 09:57:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ with st.sidebar:
 
     tab1, tab2 = st.tabs(["Chat with the File", "About the Application"])
 
-   with tab1: 
+with tab1:
     st.write('To obtain an API Key you must create an OpenAI account at the following link: https://openai.com/api/')
     if 'generated' not in st.session_state:
         st.session_state['generated'] = []
